88b9542efd33bafc5f83ae687d9fb79d574ca244
Kaung Htet Hein <kaunghtethein27801@gmail.com>
2023-03-03 22:27:50 +0630

    Updated the whole project

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const https = require('https');
+const { checkServerIdentity } = require('tls');
 const app = express();
 
+
 require('dotenv').config();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
 const apiKey = process.env.API_KEY;
 
-app.get("/",function(request,response){
-    // response.sendFile(__dirname+"/index.html");
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Yangon&appid="+apiKey;
-    https.get(url,function(res){
-        res.on("data",function(data){
+
+app.get("/", function (request, response) {
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+"Yangon"+"&appid=" + apiKey+"&units=metric";
+    https.get(url, function (res) {
+        res.on("data", function (data) {
             const weatherData = JSON.parse(data);
-            const imgUrl = "http://openweathermap.org/img/wn/" +weatherData.weather[0].icon+"@2x.png";
-           
-            response.write("<div><img src="+imgUrl+"></div>");
-            response.write("<h1>The temperature in Yangon is "+ weatherData.main.temp +" Kelvins.</h1>")
-            response.send();
+            var weatherEJS = {
+                cityName: "Yangon",
+                temp: weatherData.main.temp
+            }
+            response.render("index",weatherEJS);
+         
+        })
+    })
+    
+})
+
+app.post("/", function(request,response){
+    const city = request.body.city;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=" + apiKey+"&units=metric";
+    https.get(url, function (res) {
+        res.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            var weatherEJS = {
+                cityName: city,
+                temp: weatherData.main.temp
+            }
+            response.render("index",weatherEJS);
+         
         })
     })
 })
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server is running at port 3000.");
 })
+
